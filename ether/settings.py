e18f26a70a9f5e62fe53d16d00958c75485ff5c8
hamza faisal <hamzafaisaljarral@gmail.com>
2020-03-20 12:55:39 +0000

    change db

--- ether/settings.py
+++ ether/settings.py
@@ -78,19 +78,14 @@ WSGI_APPLICATION = 'ether.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+
 
 
 # Password validation
